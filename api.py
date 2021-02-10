d6a191025104e82be6ac3cbf06af070dbcf08540
unknown <leonardo.sanchezsoler@gmail.com>
2021-02-10 12:09:43 +0100

    third code pasted

--- api.py
+++ api.py
@@ -7,3 +7,11 @@ def landing_page():
  return render_template('index.html')
 if __name__ == '__main__':
  app.run(host='0.0.0.0',port=1991, debug=True)
+
+@app.route("/dashboard")
+def dashboard():
+ return render_template('dashboard.html')
+ 
+@app.route("/map")
+def map():
+ return render_template('map.html')
